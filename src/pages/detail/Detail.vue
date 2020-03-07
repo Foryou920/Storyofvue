9c5df8721ce2f9b66bc492c331487f5e2d3b4138
遇见 <30545603+Foryou920@users.noreply.github.com>
2020-03-07 21:32:25 +0800

    Detail-Final

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,21 +2,54 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content"><detail-list :list="list"></detail-list></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from "./components/banner";
-import DetailHeader from "./components/header"
+import DetailHeader from "./components/header";
+import DetailList from "./components/list";
 
 export default {
   name: "Detail",
   components: {
     DetailBanner: DetailBanner,
-    DetailHeader: DetailHeader
+    DetailHeader: DetailHeader,
+    DetailList: DetailList
+  },
+  data: function() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [
+            {
+              title: "成人三馆联票",
+              children: [
+                {
+                  title: "成人三馆联票 - 某一连锁店销售"
+                }
+              ]
+            },
+            {
+              title: "成人五馆联票"
+            }
+          ]
+        },
+        {
+          title: "学生票"
+        },
+        {
+          title: "儿童票"
+        },
+        {
+          title: "特惠票"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
